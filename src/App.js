5a43eb42293b254575f7697ab900db61e4538af3
andres145437 <andresenriquez893@yahoo.com>
2023-01-28 12:14:03 -0800

    updating app.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import './App.css';
 //import axios from "axios";
 
 function App() {
-  
-  // const [Games,setGames]= useState([]);
-  // const [Image,setImage]= useState([]);
-  // const [video,setVideo]= useState([]);
-  // const [leagues,setLeagues]= useState([]);
+
 
   return (
     <div className="App">
